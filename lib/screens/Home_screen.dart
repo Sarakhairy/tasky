2e330012bbe842e06601eabc2a87edda74233bb6
SaraKhairy <sarahkhairy180@gmail.com>
2025-09-14 16:41:00 +0200

    finishing achied task section and finishing profile screen ui

--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -16,6 +17,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String name = 'Default';
   dynamic task = [];
+  int totalTasks = 0;
+  int doneTasks = 0;
+  double percent = 0;
   @override
   void initState() {
     _loadTasks();
@@ -34,12 +38,21 @@ class _HomeScreenState extends State<HomeScreen> {
     final finalTask = prefs.getString("tasks");
     if (finalTask != null) {
       final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
+
       setState(() {
         task = taskAfterDecode.map((e) {
           return TaskModel.fromJson(e);
         }).toList();
+        _calculateProgress();
       });
+
     }
+  }
+
+  _calculateProgress() {
+    totalTasks = task.length;
+    doneTasks = task.where((element) => element.isCompleted == true).length;
+    percent = totalTasks == 0 ? 0 : doneTasks / totalTasks;
   }
 
   @override
@@ -64,71 +77,132 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.grey[200],
       body: SafeArea(
         child: task.isEmpty
-          ? Center(child: Text("No Tasks Added Yet"))
-          : Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/avatar.png"),
-                  ),
-                  SizedBox(width: 8),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Good Evening, $name",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
+            ? Center(child: Text("No Tasks Added Yet"))
+            : Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                            "assets/images/avatar.png",
+                          ),
                         ),
-                      ),
-                      Text(
-                        "One task at a time. One step closer.",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
+                        SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Good Evening, $name",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            Text(
+                              "One task at a time. One step closer.",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
                         ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Yuhuu, Your work is\nalmost done",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    SizedBox(height: 16),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(color: Colors.grey.shade300),
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Text(
-                "Yuhuu, Your work is\nalmost done",
-                style: TextStyle(fontSize: 32),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  padding: EdgeInsets.only(bottom: 60),
-                  itemCount: task.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.only(top: 8),
-                      child: TaskWidget(
-                        task: task[index],
-                        onChanged: (value) async {
-                          setState(() {
-                            task[index].isCompleted = value ?? false;
-                          });
-                          final prefs = await SharedPreferences.getInstance();
-                          final String encodedData = jsonEncode(
-                            task.map((e) => e.toJson()).toList(),
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Achieved Tasks",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                "$doneTasks Out of $totalTasks Done",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Transform.rotate(
+                                angle: -pi / 2,
+                                child: CircularProgressIndicator(
+                                  value: percent,
+                                  backgroundColor: Color(0xFF6D6D6D),
+                                  valueColor: AlwaysStoppedAnimation(
+                                    Color(0xFF15B86C),
+                                  ),
+                                  strokeWidth: 4,
+                                ),
+                              ),
+                              Text(
+                                "${percent * 100 ~/ 1}%",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: EdgeInsets.only(bottom: 60),
+                        itemCount: task.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(top: 8),
+                            child: TaskWidget(
+                              task: task[index],
+                              onChanged: (value) async {
+                                setState(() {
+                                  task[index].isCompleted = value ?? false;
+                                  _calculateProgress();
+                                });
+                                final prefs =
+                                    await SharedPreferences.getInstance();
+                                final String encodedData = jsonEncode(
+                                  task.map((e) => e.toJson()).toList(),
+                                );
+                                await prefs.setString("tasks", encodedData);
+                              },
+                            ),
                           );
-                          await prefs.setString("tasks", encodedData);
                         },
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
       ),
     );
   }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String name = "Default";
+  bool isDarkMode = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadData();
+  }
+
+  void _loadData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    name = prefs.getString("username") ?? '';
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("To Do Tasks"), centerTitle: true),
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: AssetImage("assets/images/avatar.png"),
+                    ),
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(color: Colors.grey[300]!, width: 2),
+                        color: Colors.white,
+                      ),
+                      child: Icon(
+                        Icons.camera_alt_outlined,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 6),
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  "One task at a time. One step closer.",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text("Profile Info", style: TextStyle(fontSize: 20)),
+            SizedBox(height: 8),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text("User Details", style: TextStyle(fontSize: 16)),
+              leading: Icon(Icons.person_outline),
+              trailing: Icon(Icons.arrow_forward_ios, size: 16),
+            ),
+            SizedBox(height: 24),
+            Divider(thickness: 1, color: Colors.grey[300]),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text("Dark Mode", style: TextStyle(fontSize: 16)),
+              leading: Icon(Icons.dark_mode_outlined),
+              trailing: Switch(
+                activeTrackColor: Color(0xFF15B86C),
+                inactiveTrackColor: Colors.white,
+                value: isDarkMode,
+                onChanged: (val) {
+                  isDarkMode = val;
+                  setState(() {
+                    
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 24),
+            Divider(thickness: 1, color: Colors.grey[300]),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text("Log Out", style: TextStyle(fontSize: 16)),
+              leading: Icon(Icons.logout_outlined),
+              trailing: Icon(Icons.arrow_forward_ios, size: 16),
+            ),
+            SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
   }
 }
